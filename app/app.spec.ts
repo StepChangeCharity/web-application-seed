f7daebaf5a8a925102288627163d817e99f0f4e0
ChrisMurphy <monkeeman69@googlemail.com>
2016-06-14 14:50:00 +0100

    feat(build): Tslint task with standard rules

--- app/app.spec.ts
+++ app/app.spec.ts
@@ -2,16 +2,14 @@ import {it, describe, expect, beforeEach, inject} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
-    let appComponent: AppComponent;
-    
-    beforeEach(() => {
-        appComponent = new AppComponent();
-    });
+	let appComponent: AppComponent;
+	beforeEach(() => {
+		appComponent = new AppComponent();
+	});
 
-    describe("AppComponent", function() {
-        it("has a name property", function() {
-            expect(appComponent.name).toBe("My First Angular 2 App");
-        });
-    });
-
+	describe('AppComponent', function() {
+		it('has a name property', function() {
+			expect(appComponent.name).toBe('My First Angular 2 App');
+		});
+	});
 });
