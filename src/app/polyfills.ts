4f2d786d7186cc70495eb5c650d48711373921c6
ChrisMurphy <monkeeman69@googlemail.com>
2016-06-22 11:43:35 +0100

    chore: simplified statement

--- src/app/polyfills.ts
+++ src/app/polyfills.ts
@@ -2,10 +2,7 @@ import 'core-js/es6';
 import 'reflect-metadata';
 require('zone.js/dist/zone');
 
-if (process.env.NODE_ENV === 'production') {
-  // Production
-} else {
-	// Development
+if (process.env.NODE_ENV !== 'production') {
 	Error['stackTraceLimit'] = Infinity;
 
 	require('zone.js/dist/long-stack-trace-zone');
