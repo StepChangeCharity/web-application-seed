8cc92bb609bae9604be860839c4533f57b5f6448
Andrew Woods <arwoods@gmail.com>
2016-07-01 15:17:59 +0100

    Testing semver version numbering with clean folder first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Component } from '@angular/core';
 			<img src='./assets/images/logo.jpg' />
 			<h1 class="title">{{name}}</h1>
 			<h2 class="sub-title">I feel the need, the need for seed!</h2>
-			<p>Give me a new version</p>
 		</div>
 	`,
 	styles: [require('./app.scss')]
