1e6277f8cc6a5dacd4bddcf8dab1ba5cb612b8bf
Franz Junge <bitbucket@junge.f9.co.uk>
2016-08-15 15:19:12 +0100

    chore(rc5): Upgrade to Angular2 RC5 - Converted bootstrap to ngModule.

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule, enableProdMode, provide, APP_INITIALIZER, ExceptionHandler }  from '@angular/core';
+import { HTTP_PROVIDERS, Http, XHRBackend }                                      from '@angular/http';
+import { BrowserModule }                                                         from '@angular/platform-browser';
+import { bootstrap }                                                             from '@angular/platform-browser-dynamic';
+import { LoggingService, LoggingErrorHandler }                                   from '../services';
+import { AppComponent }                                                          from './app.component';
+import { AppConfig }                                                             from './app-config';
+import { ConfigurationService }                                                  from './configuration-service';
+import '../theme/styles.scss';
+
+if (process.env.NODE_ENV === 'production') {
+	// "enableProdMode" _has_ to be called before bootstrapping the application
+	// ... so no choice but to do it as part of the build.
+	enableProdMode();
+}
+
+
+@NgModule({
+	imports: [
+		BrowserModule
+	],
+	declarations: [
+		AppComponent
+	],
+	bootstrap: [
+		AppComponent
+	],
+	providers: [
+		AppConfig,
+		ConfigurationService,
+		LoggingService,
+		HTTP_PROVIDERS,
+		provide( ExceptionHandler, {useClass: LoggingErrorHandler} ),
+		provide(APP_INITIALIZER, {
+			useFactory: (configurationService: ConfigurationService) => () => {
+				return configurationService.loadConfiguration('./app.config.json');
+			},
+			deps: [ConfigurationService, HTTP_PROVIDERS],
+			multi: true
+		})
+	]
+})
+export class AppModule {
+
+}
